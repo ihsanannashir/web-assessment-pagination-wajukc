cff64edd110c75e6afb5ffa3c29157c7be1c4564
ihsanannashir <64590661+ihsanannashir@users.noreply.github.com>
2023-06-06 17:31:53 +0700

    finish pagination

--- src/App.js
+++ src/App.js
@@ -1,15 +1,115 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import Card from './components/Card';
-import Pagination from './components/Pagination';
+import data from './data/data';
+
+const firstData = data.filter((item) => {
+  return item.id === 1;
+});
 
 export const App = () => {
+  const [current, setCurrent] = useState(firstData);
+  const [currentId, setCurrentId] = useState(1);
+
+  const getCurrent = (currentId) => {
+    const newData = data.filter((item) => {
+      return item.id === currentId;
+    });
+    setCurrent(newData);
+  };
+
+  const Pagination = () => {
+    return (
+      <div className="pagination">
+        <button
+          name="start"
+          onClick={() => {
+            if (currentId > 1) {
+              setCurrentId(1);
+              getCurrent(1);
+            }
+          }}
+          disabled={currentId === 1 && true}
+        >
+          {'<<'}
+        </button>
+        <button
+          name="prev"
+          className="prevbtn"
+          onClick={() => {
+            if (currentId > 1) {
+              setCurrentId(currentId - 1);
+              getCurrent(currentId - 1);
+            } else {
+              setCurrentId(data.length);
+              getCurrent(data.length);
+            }
+          }}
+        >
+          Prev
+        </button>
+        {data.map((x, i) => {
+          return (
+            <button
+              key={i}
+              onClick={() => {
+                getCurrent(x.id);
+                setCurrentId(x.id);
+              }}
+            >
+              {x.id}
+            </button>
+          );
+        })}
+        <button
+          name="next"
+          className="nextbtn"
+          onClick={() => {
+            if (currentId < data.length) {
+              setCurrentId(currentId + 1);
+              getCurrent(currentId + 1);
+            } else {
+              setCurrentId(1);
+              getCurrent(1);
+            }
+          }}
+        >
+          Next
+        </button>
+        <button
+          name="end"
+          onClick={() => {
+            if (currentId < data.length) {
+              setCurrentId(data.length);
+              getCurrent(data.length);
+            }
+          }}
+          disabled={currentId === data.length && true}
+        >
+          {'>>'}
+        </button>
+      </div>
+    );
+  };
+
   return (
     <div className="container">
-      <Card />
+      {current.map((item, idx) => {
+        return (
+          <Card
+            title={`Card ${item.id}`}
+            image={item.image.url}
+            desc={item.description}
+          />
+        );
+      })}
       <Pagination />
     </div>
   );
 };
 
 export default App;
+
+// {data.filter((item) => {
+//   return item.id === x
+// })}
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 
-export const Card = ({ data }) => {
+export const Card = (props) => {
   return (
     <div className="card">
+      <div className="thumbnail">
+        <h2 className="title">{props.title ? props.title : 'Card'}</h2>
+        <img
+          src={
+            props.image
+              ? props.image
+              : 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=8'
+          }
+        />
+      </div>
+      <p className="description">{props.desc && props.desc}</p>
+    </div>
+  );
+};
+
+export default Card;
+
+{
+  /* <div className="card">
       <div className="thumbnail">
         <h2 className="title">Card 1</h2>
         <img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=8" />
@@ -13,8 +32,5 @@ export const Card = ({ data }) => {
         voluptates necessitatibus, possimus minus perspiciatis expedita totam
         consequatur
       </p>
-    </div>
-  );
-};
-
-export default Card;
+    </div> */
+}
